64ff5c0f335dc9ea31796f3556d6b79f421f6875
YolandaTain <yolandataingut@gmail.com>
2024-01-23 10:27:19 +0100

    nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,23 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpresaComponent } from './empresa/empresa.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { ModelsComponent } from './models/models.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpresaComponent,
+    EmpleadoComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    //provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
